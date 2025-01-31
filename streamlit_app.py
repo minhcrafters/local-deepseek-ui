485c7b28de3ad4f75b99d4ddc82fe58e6936b5ed
Michael Le <59521026+minhcrafters@users.noreply.github.com>
2025-01-31 08:07:43 +0000

    afs

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ with st.sidebar:
     else:
         selected_params = st.selectbox(
             "Choose Params Size",
-            ["1.5b", "7b", "8b", "14b", "32b", "671b"],
+            ["1.5b", "7b", "8b", "14b", "32b"],
             index=0
         )
         final_model = f"{selected_model}:{selected_params}"
